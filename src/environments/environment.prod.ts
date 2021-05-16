6c1cec0a214f49c1adc2610b5f64a0d9d8841a4f
Huan-Chieh Liao <liao.jason2@gmail.com>
2021-05-16 15:57:13 +0800

    Move Firebase key to prodution

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "",
+    authDomain: "gcp2021test.firebaseapp.com",
+    projectId: "gcp2021test",
+    storageBucket: "gcp2021test.appspot.com",
+    messagingSenderId: "367981118250",
+    appId: "",
+    measurementId: ""
+  }
 };
