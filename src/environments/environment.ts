0ff49b0e44596f802b699704cdca2a50cec1e935
Huan-Chieh Liao <liao.jason2@gmail.com>
2021-05-16 08:19:11 +0800

    Add firebase key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyAbi0ZLf_BxZf8-EFkfM09dQD_UDbleyaA",
+    authDomain: "gcp2021test.firebaseapp.com",
+    projectId: "gcp2021test",
+    storageBucket: "gcp2021test.appspot.com",
+    messagingSenderId: "367981118250",
+    appId: "1:367981118250:web:1c6d4390242298e8e8d453",
+    measurementId: "G-7S7K7BBS8V"
+  }
 };
 
 /*
