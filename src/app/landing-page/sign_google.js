090f91d180ffa2e11fbcac0438f683697008f71a
Alec Wu <980124jack@gmail.com>
2021-05-16 09:20:42 +0800

    Create sign_google.js

---
+++ src/app/landing-page/sign_google.js
@@ -0,0 +1,15 @@
+const signInWithGoogleButton = document.getElementById("signInWithGoogle");
+
+const auth = firebase.auth();
+
+const signInWithGoogle = () => {
+    const googleProv = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(googleProv)
+    .then(() => {
+        window.location.assign('src/app/user-register/user-register.component.html');
+    })
+    .catch(error => {
+        console.error(error);
+    })
+}
