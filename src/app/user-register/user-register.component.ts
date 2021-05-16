042bf2c71f59d8f65fbd24ace9129f7161e88e28
Huan-Chieh Liao <liao.jason2@gmail.com>
2021-05-16 15:24:02 +0800

    DSC Final

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, NgZone} from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
 import Userdata from 'src/app/models/userdata.model'
 import { UserDataService } from "src/app/services/userdata.service"
+import { AuthService } from "../shared/services/auth.service";
+import { Router } from "@angular/router";
+
+
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
@@ -12,13 +16,32 @@ export class UserRegisterComponent implements OnInit {
   userdata: Userdata = new Userdata()
   form: FormGroup;
   submitted = false;
-  constructor(private userdataservice: UserDataService) { }
+  constructor(
+    public authService: AuthService,
+    private userdataservice: UserDataService,
+    public router: Router,
+    public ngZone: NgZone
+    ) { }
 
   ngOnInit(): void {
+    /*this.form = this.formBuilder.group(
+      {
+        name: ['', Validators.required],
+        phone: ['', Validators.pattern("^[0-9][1-9]{9}")],
+        nic: ['', Validators.pattern("^[A-Z][0-9]{9}"],
+      }*/
   }
+
+  /*get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }*/
+
 
   saveUserdata(): void {
     this.userdataservice.create(this.userdata).then(() => {
+      if (this.form.invalid) {
+        return;
+      }
       console.log("Created item Successfully")
       this.submitted = true;
     })
